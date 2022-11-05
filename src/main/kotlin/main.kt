2b8f5c4e2f08d67a2d90179d9c3d73f11d4888ea
Ivan Melnikov <johnny-mill@yandex.ru>
2022-11-05 17:19:57 +0300

    Add day and month limits

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,13 +9,59 @@ const val CARD_TYPE_VKPAY = "Vk Pay"
 
 const val ERROR_UNKNOWN_CARD = -1
 
+const val ERROR_LIMIT_OK = 0
+const val ERROR_LIMIT_ONE_OPERATION = -10
+const val ERROR_LIMIT_DAY = -11
+const val ERROR_LIMIT_MONTH = -12
+
 fun main() {
-    calcCommission(10_000)
-    calcCommission(10_000, card = CARD_TYPE_MASTERCARD)
-    calcCommission(10_000, 100_000, CARD_TYPE_MAESTRO)
-    calcCommission(10_000, card = CARD_TYPE_VISA)
-    calcCommission(1_000, 100_000, CARD_TYPE_MIR)
-    calcCommission(100, card = "fake")
+    testTransfer(100, CARD_TYPE_MASTERCARD)
+    testTransfer(100, CARD_TYPE_MAESTRO)
+    testTransfer(100, CARD_TYPE_VISA)
+    testTransfer(100, CARD_TYPE_MIR)
+    testTransfer(100, CARD_TYPE_VKPAY)
+
+    transfer(0, 0, 0, "fake")
+}
+
+fun testTransfer(amountStart: Int, card: String) {
+    var amountDay = 0
+    var amountMonth = 0
+    var amount = amountStart
+
+    println(">>> Test transfers with '$card'")
+
+    for (i in 1..10)
+    {
+        if (i % 6 == 0) {
+            // Next day
+            amount = amountStart
+            amountDay = 0
+        }
+        if (transfer(amount, amountDay, amountMonth, card)) {
+            amountDay += amount
+            amountMonth += amount
+            amount *= 10
+        }
+    }
+
+    println()
+}
+
+fun transfer(amount: Int, amountDay: Int, amountMonth: Int, card: String = CARD_TYPE_VKPAY): Boolean {
+    var success = false
+    when (checkLimit(amount, amountDay, amountMonth, card)) {
+        ERROR_LIMIT_OK -> {
+            val commission = calcCommission(amount, amountMonth, card)
+            println("$card: transfer $amount RUB with commission $commission RUB")
+            success = true
+        }
+        ERROR_UNKNOWN_CARD -> println("Unknown card '$card'")
+        ERROR_LIMIT_ONE_OPERATION -> println("$card: Limit for one operation exceeded")
+        ERROR_LIMIT_DAY -> println("$card: Day limit exceeded")
+        ERROR_LIMIT_MONTH -> println("$card: Month limit exceeded")
+    }
+    return success
 }
 
 fun calcCommission(amount: Int, amountMonth: Int = 0, card: String = CARD_TYPE_VKPAY): Int {
@@ -29,11 +75,33 @@ fun calcCommission(amount: Int, amountMonth: Int = 0, card: String = CARD_TYPE_V
             if (300 <= limit && limit <= 75_000) 0 else 20 + (amount * 0.006f).roundToInt()
         }
         else -> {
-            // println("Unknown card '$card'")
             ERROR_UNKNOWN_CARD
         }
     }
 
-    // println("$card: transfer $amount RUB with commission $commission RUB")
     return commission
+}
+
+fun checkLimit(amount: Int, amountDay: Int, amountMonth: Int, card: String = CARD_TYPE_VKPAY): Int {
+    val status = when (card) {
+        CARD_TYPE_VKPAY -> {
+            when {
+                amount > 15_000 -> ERROR_LIMIT_ONE_OPERATION
+                amount + amountMonth > 40_000 -> ERROR_LIMIT_MONTH
+                else -> ERROR_LIMIT_OK
+            }
+        }
+        CARD_TYPE_VISA, CARD_TYPE_MIR, CARD_TYPE_MASTERCARD, CARD_TYPE_MAESTRO -> {
+            when {
+                amount + amountDay > 150_000 -> ERROR_LIMIT_DAY
+                amount + amountMonth > 600_000 -> ERROR_LIMIT_MONTH
+                else -> ERROR_LIMIT_OK
+            }
+        }
+        else -> {
+            ERROR_UNKNOWN_CARD
+        }
+    }
+
+    return status
 }
